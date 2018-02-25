b99b72de5b8734b05287e6966141859b3c987fe3
michaelvdv <michael@ubuntu.basic>
2018-02-25 14:13:56 +0000

    changed some things

--- task1.py
+++ task1.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import csv
-from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-from sklearn.tree import DecisionTreeClassifier
+#from sklearn.naive_bayes import GaussianNB
+#from sklearn.model_selection import train_test_split
+#from sklearn.metrics import accuracy_score
+#from sklearn.metrics import confusion_matrix
+#from sklearn.tree import DecisionTreeClassifier
 
 def run():
-    df = pd.read_csv('sepsis.csv')
-
-print df
+    df = pd.read_csv2('sepsis.csv')
+    print df
 
 run()
